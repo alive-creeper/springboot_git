7e2cda5031dfa47129ab5fda47b94194742b3d3a
zs <zs@bjpowernode.com>
2020-11-30 15:21:53 +0800

    Initial commit

---
+++ src/main/java/com/entor/controller/HelloController.java
@@ -0,0 +1,10 @@
+package com.entor.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    public String hello(){
+        return "";
+    }
+}
